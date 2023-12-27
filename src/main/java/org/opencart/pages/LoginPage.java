508e42fcd0517b98370462a0c224e9182c2914cd
LaVanda <vandaserduk@gmail.com>
2023-12-27 18:46:12 -0500

    Пример написсания BDD

---
+++ src/main/java/org/opencart/pages/LoginPage.java
@@ -0,0 +1,13 @@
+package org.opencart.pages;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
